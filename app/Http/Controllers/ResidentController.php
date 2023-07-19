5e7c3c1d061a4f066a0ff41edcb669fe6e47164c
paulAlas <paul4145irvin@gmail.com>
2023-07-19 18:58:17 +0800

    changed cols in ehr,PE added contrllrs for both

--- app/Http/Controllers/ResidentController.php
+++ app/Http/Controllers/ResidentController.php
@@ -6,10 +6,7 @@ use App\Models\resident;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use Hash;
-use Illuminate\Http\Response;
-use Auth;
-use Validator;
+
 class ResidentController extends Controller
 {
            
@@ -114,35 +111,6 @@ class ResidentController extends Controller
         return response('updated');
     }
 
-    public function loginResident(request $request){
-        $request->validate([
-            'resident_userName' => 'required',
-            'resident_password' => 'required',
-        ]);
-
-        $resInfo = resident::where('resident_userName', $request->resident_userName)->first();
-
-        if(!$resInfo){
-            return back()->with('fail', 'unknown username');
-        }else{
-            if(Hash::check($request->resident_password, $resInfo->resident_password)){
-                    $request->session()->put('loggedUser', $resInfo->resident_id);
-                    return redirect(route('departments.index'));
-
-
-            }else{
-                return back()->with('fail', 'wrong password');
-            }
-        }
-
-    }
-
-
-    public function logoutRes(){
-        if(Session()->has('loggedUser')){
-            session()->pull('loggedUser');
-            return redirect(route('login'));
-        }
-    }
+  
    
 }
