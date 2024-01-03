0f34ea2832fc31243ed847bea4d9a402be487667
paulAlas <paul4145irvin@gmail.com>
2024-01-04 02:56:17 +0800

    rewriting names 9

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,19 +2,18 @@
 #!/usr/bin/env bash
 
 echo "Running composer"
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
-
-echo "Updating composer"
-composer update --no-scripts --working-dir=/var/www/html
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/htm
 
 echo "Running composer dump-autoload"
 composer dump-autoload --working-dir=/var/www/html
 
+echo "Caching config..."
+php artisan config:cache
+
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Caching config..."
-php artisan config:cache
+
 
 
 
