7e7bac7f72d30bcf103620c184ab3e2981dc8538
paulAlas <paul4145irvin@gmail.com>
2024-01-03 10:29:21 +0800

    trying changes for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,9 @@
 echo "Running composer"
 composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
+echo "Updating composer"
+composer update --no-scripts --working-dir=/var/www/html
+
 echo "Running composer dump-autoload"
 composer dump-autoload --working-dir=/var/www/html
 
@@ -14,3 +17,4 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
