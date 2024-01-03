e56135444679b54dec3dda84b2c4371a872bf865
paulAlas <paul4145irvin@gmail.com>
2024-01-04 06:02:57 +0800

    routes cache

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,6 +13,9 @@ composer dump-autoload --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
 
+echo "Caching routes..."
+php artisan route:cache
+
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
 
