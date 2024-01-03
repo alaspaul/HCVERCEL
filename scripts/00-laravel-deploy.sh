e9ac26fdb5759329e33b14db994b9d96f2447554
paulAlas <paul4145irvin@gmail.com>
2024-01-04 03:52:24 +0800

    clearing cache

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -14,7 +14,7 @@ echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
-php artisan route:
+php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
