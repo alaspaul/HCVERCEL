ef8575f614257b9ed0ff9f8c69f15636e161e202
paulAlas <paul4145irvin@gmail.com>
2024-01-03 23:29:04 +0800

    update on env

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,11 @@
 
 #!/usr/bin/env bash
 
-# echo "Running composer"
-# composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+echo "Running composer"
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-# echo "Updating composer"
-# composer update --no-scripts --working-dir=/var/www/html
+echo "Updating composer"
+composer update --no-scripts --working-dir=/var/www/html
 
 echo "Running composer dump-autoload"
 composer dump-autoload --working-dir=/var/www/html
