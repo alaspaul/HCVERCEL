0ced18af8c10ff764fd2431768929e14689e4a43
paulAlas <paul4145irvin@gmail.com>
2024-01-03 23:25:34 +0800

    changes on deploy kernel and cors

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,11 @@
 
 #!/usr/bin/env bash
 
-echo "Running composer"
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+# echo "Running composer"
+# composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-echo "Updating composer"
-composer update --no-scripts --working-dir=/var/www/html
+# echo "Updating composer"
+# composer update --no-scripts --working-dir=/var/www/html
 
 echo "Running composer dump-autoload"
 composer dump-autoload --working-dir=/var/www/html
@@ -16,10 +16,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-
-
-echo "Running migrations..."
-php artisan migrate --force --seed
+# echo "Running migrations..."
+# php artisan migrate --force --seed
 
 
 
