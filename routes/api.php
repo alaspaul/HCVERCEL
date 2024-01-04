3cb975e89e3f865f6089c3fa5098537e56497506
paulAlas <paul4145irvin@gmail.com>
2024-01-04 21:08:47 +0800

    api routes Case revision

--- routes/api.php
+++ routes/api.php
@@ -45,19 +45,19 @@ use App\Http\Controllers\VitalController;
 
 
 
-Route::post('/admin/Login', [UserController::class, 'loginAdmin'])->name('admin.login');
+Route::POST('/admin/Login', [UserController::class, 'loginAdmin'])->name('admin.login');
 
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:adminApi'], function () {
-    Route::get('Check', [UserController::class, 'checker'])->name('checker');
-    Route::get('Logout', [UserController::class, 'logoutAdmin'])->name('logout');
+    Route::GET('Check', [UserController::class, 'checker'])->name('checker');
+    Route::GET('Logout', [UserController::class, 'logoutAdmin'])->name('logout');
     Route::apiResource('admin', UserController::class);
 
 
     
     Route::apiResource('resActLog', ResActionLogController::class);
-    Route::get('resActLog/residentName/{resident_id}', [ResActionLogController::class, 'residentName'])->name('RAL.residentName');
-    Route::get('resActLog/logs/department', [ResActionLogController::class, 'logsByDep'])->name('RAL.logsByDep');
+    Route::GET('resActLog/residentName/{resident_id}', [ResActionLogController::class, 'residentName'])->name('RAL.residentName');
+    Route::GET('resActLog/logs/department', [ResActionLogController::class, 'logsByDep'])->name('RAL.logsByDep');
 
 
 
@@ -83,35 +83,35 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin
     Route::POST('rooms/edit{room}', [RoomController::class, 'edit'])->name('rooms.edit');
     Route::POST('rooms/updateRoom{room}', [RoomController::class, 'updateRoom'])->name('rooms.updateRoom');
     Route::POST('rooms/getRooms{roomId}', [RoomController::class, 'getRoom'])->name('rooms.getRoom');
-    Route::post('rooms/getRoomsByfloor/{floor_id}', [RoomController::class, 'getRoomByFloor'])->name('room.getRoomByFloor');
+    Route::POST('rooms/getRoomsByfloor/{floor_id}', [RoomController::class, 'getRoomByFloor'])->name('room.getRoomByFloor');
 
 });
 
 
 
 
-Route::post('/login', [loginController::class, 'loginResident'])->name('loginRes');
+Route::POST('/login', [loginController::class, 'loginResident'])->name('loginRes');
 
 
 Route::group(['middleware' => 'auth:customApi'],function(){
-    Route::get('/logout', [loginController::class, 'logoutRes'])->name('logout');
+    Route::GET('/logout', [loginController::class, 'logoutRes'])->name('logout');
 
 
     Route::apiResource('rooms', RoomController::class);
     Route::POST('rooms/edit{room}', [RoomController::class, 'edit'])->name('rooms.edit');
     Route::POST('rooms/updateRoom{room}', [RoomController::class, 'updateRoom'])->name('rooms.updateRoom');
     Route::POST('rooms/getRooms{roomId}', [RoomController::class, 'getRoom'])->name('rooms.getRoom');
-    Route::post('rooms/getRoomsByfloor/{floor_id}', [RoomController::class, 'getRoomByFloor'])->name('room.getRoomByFloor');
+    Route::POST('rooms/getRoomsByfloor/{floor_id}', [RoomController::class, 'getRoomByFloor'])->name('room.getRoomByFloor');
 
     
     Route::apiResource('fileUpload', FileUploadController::class);
     Route::GET('fileUpload/download/{file_id}', [FileUploadController::class, 'download'])->name('fileUpload.download');
-    Route::post('fileUpload/getFiles', [FileUploadController::class, 'getFiles'])->name('fileUpload.getFiles');
-    Route::get('fileUpload/viewFile/{file_id}', [FileUploadController::class, 'viewFile'])->name('fileUpload.viewFile');
+    Route::POST('fileUpload/getFiles', [FileUploadController::class, 'getFiles'])->name('fileUpload.getFiles');
+    Route::GET('fileUpload/viewFile/{file_id}', [FileUploadController::class, 'viewFile'])->name('fileUpload.viewFile');
 
     Route::apiResource('resActLog', ResActionLogController::class);
-    Route::get('resActLog/residentName/{resident_id}', [ResActionLogController::class, 'residentName'])->name('RAL.residentName');
-    Route::get('resActLog/logs/department', [ResActionLogController::class, 'logsByDep'])->name('RAL.logsByDep');
+    Route::GET('resActLog/residentName/{resident_id}', [ResActionLogController::class, 'residentName'])->name('RAL.residentName');
+    Route::GET('resActLog/logs/department', [ResActionLogController::class, 'logsByDep'])->name('RAL.logsByDep');
 
 
 
@@ -142,7 +142,7 @@ Route::group(['middleware' => 'auth:customApi'],function(){
 
 
     Route::apiResource('departments', DepartmentController::class);
-    Route::get('departments/getDepNamebyId/{department_id}', [DepartmentController::class, 'getDepNamebyId'])->name('departments.getDepNamebyId');
+    Route::GET('departments/getDepNamebyId/{department_id}', [DepartmentController::class, 'getDepNamebyId'])->name('departments.getDepNamebyId');
   
 
     Route::apiResource('floors', FloorController::class);
@@ -150,34 +150,34 @@ Route::group(['middleware' => 'auth:customApi'],function(){
     Route::POST('floors/updateFloor{floor}', [FloorController::class, 'updateFloor'])->name('floors.updateFloor');
 
     Route::apiResource('patients', PatientController::class);
-    Route::get('patients/getPatientName/{id}', [PatientController::class, 'getPatientName'])->name('patients.getPatientName');
-    Route::get('patients/getPatientbyId/{id}', [PatientController::class, 'getPatientbyId'])->name('patients.getPatientbyId');
-    Route::get('patients/getPatientRoom/{id}', [PatientController::class, 'getPatientRoom'])->name('patients.getPatientRoom');
-    Route::post('patients/addPhr/{id}', [PatientController::class, 'addPhr'])->name('patients.addPhr');
+    Route::GET('patients/getPatientName/{id}', [PatientController::class, 'getPatientName'])->name('patients.getPatientName');
+    Route::GET('patients/getPatientbyId/{id}', [PatientController::class, 'getPatientbyId'])->name('patients.getPatientbyId');
+    Route::GET('patients/getPatientRoom/{id}', [PatientController::class, 'getPatientRoom'])->name('patients.getPatientRoom');
+    Route::POST('patients/addPhr/{id}', [PatientController::class, 'addPhr'])->name('patients.addPhr');
 
 
     Route::apiResource('formCategories', PhrFormCategoriesController::class);
 
     Route::apiResource('categoryAttributes', PhrCategoryAttributesController::class);
-    Route::get('categoryAttributes/getAttributeName/{id}', [PhrCategoryAttributesController::class, 'getAttributeName'])->name('categoryAttributes.getAttributeName');
+    Route::GET('categoryAttributes/getAttributeName/{id}', [PhrCategoryAttributesController::class, 'getAttributeName'])->name('categoryAttributes.getAttributeName');
 
     Route::apiResource('attributeValues', PhrAttributeValuesController::class);
-    Route::get('attributeValues/getPHR/{patient_id}', [PhrAttributeValuesController::class, 'getPHR'])->name('attributeValues.getPHR');
-    Route::get('attributeValues/getAttributeName/{id}', [PhrAttributeValuesController::class, 'getAttributeName'])->name('attributeValues.getAttributeName');
-    Route::get('attributeValues/getPhrDates/{patient_id}', [PhrAttributeValuesController::class, 'getPhrDates'])->name('attributeValues.getPhrDates');
-    Route::get('attributeValues/getPhrbyDate/{patient_id}', [PhrAttributeValuesController::class, 'getPhrbyDate'])->name('attributeValues.getPhrbyDate');
-    Route::get('attributeValues/comparePhr/{patient_id}', [PhrAttributeValuesController::class, 'comparePhr'])->name('attributeValues.comparePhr');
-    Route::get('attributeValues/getPHRM/{patient_id}', [PhrAttributeValuesController::class, 'getPHRM'])->name('attributeValues.getPHRM');
+    Route::GET('attributeValues/getPHR/{patient_id}', [PhrAttributeValuesController::class, 'getPHR'])->name('attributeValues.getPHR');
+    Route::GET('attributeValues/getAttributeName/{id}', [PhrAttributeValuesController::class, 'getAttributeName'])->name('attributeValues.getAttributeName');
+    Route::GET('attributeValues/getPhrDates/{patient_id}', [PhrAttributeValuesController::class, 'getPhrDates'])->name('attributeValues.getPhrDates');
+    Route::GET('attributeValues/getPhrbyDate/{patient_id}', [PhrAttributeValuesController::class, 'getPhrbyDate'])->name('attributeValues.getPhrbyDate');
+    Route::GET('attributeValues/comparePhr/{patient_id}', [PhrAttributeValuesController::class, 'comparePhr'])->name('attributeValues.comparePhr');
+    Route::GET('attributeValues/getPHRM/{patient_id}', [PhrAttributeValuesController::class, 'getPHRM'])->name('attributeValues.getPHRM');
 
 
     Route::apiResource('patAssRooms', PatAssRoomController::class);
-    Route::put('patAssRooms/transferPatient/{patient_id}', [PatAssRoomController::class, 'transferPatient'])->name('patAssRooms.transferPatient');
-    Route::get('patAssRooms/get/AvailableRooms', [PatAssRoomController::class, 'getAvailableRooms'])->name('patAssRooms.getAvailableRooms');
-    Route::get('patAssRooms/getPatientbyRoom/{room_id}', [PatAssRoomController::class, 'getPatientbyRoom'])->name('patAssRooms.getPatientbyRoom');
-    Route::get('patAssRooms/checkout/{patient_id}', [PatAssRoomController::class, 'checkout'])->name('patAssRooms.checkout');
-    Route::get('patAssRooms/getPatient/{patient_id}', [PatAssRoomController::class, 'getPatient'])->name('patAssRooms.getPatient');
-    Route::get('patAssRooms/getRoombyPatient/{patient_id}', [PatAssRoomController::class, 'getRoombyPatient'])->name('patAssRooms.getRoombyPatient');
-    Route::get('patAssRooms/get/unassignedRooms', [PatAssRoomController::class, 'unassignedRooms'])->name('patAssRooms.unassignedRooms');
+    Route::PUT('patAssRooms/transferPatient/{patient_id}', [PatAssRoomController::class, 'transferPatient'])->name('patAssRooms.transferPatient');
+    Route::GET('patAssRooms/get/AvailableRooms', [PatAssRoomController::class, 'getAvailableRooms'])->name('patAssRooms.getAvailableRooms');
+    Route::GET('patAssRooms/getPatientbyRoom/{room_id}', [PatAssRoomController::class, 'getPatientbyRoom'])->name('patAssRooms.getPatientbyRoom');
+    Route::GET('patAssRooms/checkout/{patient_id}', [PatAssRoomController::class, 'checkout'])->name('patAssRooms.checkout');
+    Route::GET('patAssRooms/getPatient/{patient_id}', [PatAssRoomController::class, 'getPatient'])->name('patAssRooms.getPatient');
+    Route::GET('patAssRooms/getRoombyPatient/{patient_id}', [PatAssRoomController::class, 'getRoombyPatient'])->name('patAssRooms.getRoombyPatient');
+    Route::GET('patAssRooms/get/unassignedRooms', [PatAssRoomController::class, 'unassignedRooms'])->name('patAssRooms.unassignedRooms');
 
 
     Route::apiResource('physicalExam/categories', PhysicalExamCategoriesController::class);
@@ -185,22 +185,22 @@ Route::group(['middleware' => 'auth:customApi'],function(){
     Route::apiResource('physicalExam/attributes', PhysicalExamAttributesController::class);
 
     Route::apiResource('physicalExam/values', PhysicalExamValuesController::class);
-    Route::get('physicalExam/values/getPE/{patient_id}', [PhysicalExamValuesController::class, 'getPE'])->name('physicalExamValues.getPE');
-    Route::get('physicalExam/values/getPEM/{patient_id}', [PhysicalExamValuesController::class, 'getPEM'])->name('physicalExamValues.getPEM');
+    Route::GET('physicalExam/values/getPE/{patient_id}', [PhysicalExamValuesController::class, 'getPE'])->name('physicalExamValues.getPE');
+    Route::GET('physicalExam/values/getPEM/{patient_id}', [PhysicalExamValuesController::class, 'getPEM'])->name('physicalExamValues.getPEM');
 
 
     Route::apiResource('chatGroup', ChatGroupController::class);
 
     
     Route::apiResource('chatGroupMessages', ChatGroupMessagesController::class);
-    Route::get('chatGroupMessages/get/GroupMessages/{chatGroup_id}', [ChatGroupMessagesController::class, 'getGroupMessages'])->name('chatGroupMessages.GroupMessages');
+    Route::GET('chatGroupMessages/get/GroupMessages/{chatGroup_id}', [ChatGroupMessagesController::class, 'getGroupMessages'])->name('chatGroupMessages.GroupMessages');
 
     
     Route::apiResource('chatGroupUsers', ChatGroupUsersController::class);
-    Route::get('chatGroupUsers/get/allGroups', [ChatGroupUsersController::class, 'allGroups'])->name('chatGroupUsers.allGroups');
-    Route::get('chatGroupUsers/get/allUsersinGroup/{chatGroup_id}', [ChatGroupUsersController::class, 'allUsersinGroup'])->name('chatGroupUsers.allUsersinGroup');
-    Route::get('chatGroupUsers/get/addResidents', [ChatGroupUsersController::class, 'addResidents'])->name('chatGroupUsers.addResidents');
-    Route::get('chatGroupUsers/get/firstAddResidents', [ChatGroupUsersController::class, 'firstAddResidents'])->name('chatGroupUsers.firstAddResidents');
+    Route::GET('chatGroupUsers/get/allGroups', [ChatGroupUsersController::class, 'allGroups'])->name('chatGroupUsers.allGroups');
+    Route::GET('chatGroupUsers/get/allUsersinGroup/{chatGroup_id}', [ChatGroupUsersController::class, 'allUsersinGroup'])->name('chatGroupUsers.allUsersinGroup');
+    Route::GET('chatGroupUsers/get/addResidents', [ChatGroupUsersController::class, 'addResidents'])->name('chatGroupUsers.addResidents');
+    Route::GET('chatGroupUsers/get/firstAddResidents', [ChatGroupUsersController::class, 'firstAddResidents'])->name('chatGroupUsers.firstAddResidents');
 
 
     Route::apiResource('histories', HistoryController::class);
