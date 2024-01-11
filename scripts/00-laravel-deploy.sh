c169412986fad8f22bb2bda6f3558e7a65f08f41
paulAlas <paul4145irvin@gmail.com>
2024-01-11 20:22:55 +0800

    vercel changes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,20 +1,20 @@
 
 #!/usr/bin/env bash
 
-echo "Running composer"
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+# echo "Running composer"
+# composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-echo "Updating composer"
-composer update --no-scripts --working-dir=/var/www/html
+# echo "Updating composer"
+# composer update --no-scripts --working-dir=/var/www/html
 
-echo "Running composer dump-autoload"
-composer dump-autoload --working-dir=/var/www/html
+# echo "Running composer dump-autoload"
+# composer dump-autoload --working-dir=/var/www/html
 
-echo "Caching config..."
-php artisan config:cache
+# echo "Caching config..."
+# php artisan config:cache
 
-echo "Caching routes..."
-php artisan route:cache
+# echo "Caching routes..."
+# php artisan route:cache
 
 # echo "Running migrations..."
 # php artisan migrate:fresh --force --seed
