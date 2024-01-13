144dfc6d0248c0b3b91bade04f84a98d82592be7
paulAlas <paul4145irvin@gmail.com>
2024-01-13 10:53:54 +0800

    commenting credentials true in api

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ use App\Http\Controllers\VitalController;
 // header('Access-Control-Allow-Origin: https://ipims-git-master-gabriel-26s-projects.vercel.app');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Credentials: true');
 
 Route::POST('/admin/Login', [UserController::class, 'loginAdmin'])->name('admin.login');
 
